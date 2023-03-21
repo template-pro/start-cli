a3b7227e91abd81ebf81a4dbacf195d78f47f038
wuxh <wxh1220@gmail.com>
2023-03-21 11:12:57 +0800

    chore: update
    replace chalk

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { fileURLToPath } from 'url';
 import path from "path";
-import chalk from "chalk";
+import c from "kleur";
 import mri from "mri";
 import { Argv } from "./type";
 
@@ -9,32 +9,32 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const resolvePath = (...arg: any[]) => path.resolve(__dirname, '..', ...arg);
 const readFileSync = (path: string) => fs.readFileSync(resolvePath(path), 'utf8');
 const pkg = JSON.parse(readFileSync('./package.json'));
+const command = Object.keys(pkg.bin ?? {})[0] ?? pkg.name;
 // const moduleName = pkg.name.replace(/^@.*\//, '')
 
 const argv = mri<Argv>(process.argv.slice(2), {
   alias: { h: 'help', v: 'version' },
-  string: ['branch', 'glob'],
 });
 
 async function main(args: Argv = argv) {
   if (args.version) {
-    console.log(`${chalk.bold(pkg.name)}: ${chalk.green('v' + pkg.version)}`);
+    console.log(`${c.bold(pkg.name)}: ${c.green('v' + pkg.version)}`);
     return;
   }
 
   if (args.help) {
     console.log(`
-    npx ${pkg.name}
+    npx ${c.bold(command)} [options]
     ----------------------------------------
-    -h, --help: show help.
-    -v, --version: show version. ${chalk.green('v' + pkg.version)}
+    -${c.bold('h')}, --help: show help.
+    -${c.bold('v')}, --version: show version. ${c.green('v' + pkg.version)}
     ----------------------------------------
-    ${chalk.bold('e.g.')} ${chalk.green(`${pkg.name} -h`)}
+    ${c.bold('e.g.')} ${c.green(`${command} -h`)} 
   `)
     return;
   }
 
-  console.log(`Welcome ${chalk.green(pkg.name)}`);
+  console.log(`Welcome ${c.green(pkg.name)}`);
 }
 
 export default main;
